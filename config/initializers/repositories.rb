955cb0d17b8234ac72dd16d79bf88cd520f3d44c
BGM <bgmp00@protonmail.com>
2020-01-12 12:28:31 -0300

    Update development logs's references

--- config/initializers/repositories.rb
+++ config/initializers/repositories.rb
@@ -1,5 +1,5 @@
 Repository.define do
-    repositories provider: :github, namespace: 'StratusNetwork' do
+    repositories provider: :github, namespace: 'Crowdinners' do
         repository :data do
             klass           Repository::Data
             repo            'Data'
@@ -8,39 +8,17 @@ Repository.define do
         end
 
         visible? true do
-            repository :projectares do
-                title           "ProjectAres"
-                description     "Our custom Bukkit plugins (such as PGM) that control matches and add network features to Minecraft"
-                repo            "projectares"
-                open?           true
-            end
-
-            repository :sportbukkit do
-                title           "SportBukkit"
-                description     "Our open source fork of Bukkit that is finely tuned for competitive Minecraft"
-                repo            "SportBukkit"
-                open?           true
-            end
-
             repository :web do
                 title           "Website"
-                description     "Our main website and backend repository"
-                repo            "web"
+                description     "Main website and backend repository"
+                repo            "Crowdinners-Web"
                 open?           true
             end
 
             repository :static do
                 title           "Data"
-                description     "Our static configuration data files for the servers"
-                repo            "data"
-                open?           true
-            end
-
-            repository :docs do
-                title           "XML Documentation"
-                description     "Our XML documentation website for defining map specific features"
-                repo            "xml"
-                branch          "gh-pages"
+                description     "Static configuration data files."
+                repo            "Crowdinners-Data"
                 open?           true
             end
         end
